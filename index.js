31aff650a7e7b314910a4b40996e84ac45040974
Abdirashidnoor <abdirashid16112@gmail.com>
2023-03-13 15:56:12 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 2000) + 1;
+
+let num3 = 18;
+let num4 = 7;
+let mod = num3 % num4;
+
+let numbers = [0, 4, 3, 20, 16, 5];
+let max = Math.max(...numbers);
+
